7a105ced0c43b7e0581a2eb96b653c862a596419
saci zakaria <sacideveloppeur@gmail.com>
2021-02-28 18:44:21 +0100

    1. Le premier programme Java

---
+++ Bonjour/App/Bonjour.java
@@ -0,0 +1,13 @@
+package App;
+
+import java.util.Scanner;
+
+public class Bonjour{
+    public static void main(String[] arg) {
+	    System.out.println("Bonjour! Qui etes vous?");
+
+        Scanner sc = new Scanner(System.in);
+        String nom = sc.next();
+        System.out.println("Bonjour  "+nom);
+    }
+}
---
+++ Salut/Salut.java
@@ -0,0 +1,5 @@
+public class Salut{
+    public static void main(String[] arg) {
+	    System.out.println("Salut");
+    }
+}
