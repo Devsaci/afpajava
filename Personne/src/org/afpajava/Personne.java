b6ab6ff586b53213b8a3d5b20269fc4afa4ee142
saci zakaria <sacideveloppeur@gmail.com>
2021-03-08 14:42:48 +0100

    Getters and Setters

--- Personne/src/org/afpajava/Personne.java
+++ Personne/src/org/afpajava/Personne.java
@@ -6,4 +6,39 @@ public class Personne {
     private String lastName;
     private String email;
 
+
+
+
+
+    public int getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(int identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
