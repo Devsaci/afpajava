8a1f38fada6a09859cc2ce71245924f3406740e3
saci zakaria <sacideveloppeur@gmail.com>
2021-03-09 09:21:02 +0100

    Class Client

--- Personne/src/org/afpajava/Main.java
+++ Personne/src/org/afpajava/Main.java
@@ -3,12 +3,20 @@ package org.afpajava;
 public class Main {
 
     public static void main(String[] args) {
-Personne james = new Personne(0,"james", "bond","unknown@anywhere.unk","rue Ottawa","Ottawa");
+Personne james = new Personne(0,"james", "bond","jame@mail.uk","rue Ottawa","Ottawa");
     System.out.println(james);
-    Emplyee emp =new Emplyee();
+    Emplyee emp =new Emplyee(1,"albert","Dupain","albDup@mail"," rue Ottawa","Ottawa",100000);
 //    emp.getEmail();
         System.out.println(emp.toString());
 
+
+        Client aClient = new Client();
+        aClient.setIdentifier( 0 );
+        aClient.setFirstName( "client1" );
+        aClient.setLastName( "CLIENT1" );
+        aClient.setEmail( "client1@mail.com" );
+
+        System.out.println( aClient );
 
     }
 }
