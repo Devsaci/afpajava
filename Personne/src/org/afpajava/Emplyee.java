014011e344a3882ea35ec83919bc3e35c2426ad6
saci zakaria <sacideveloppeur@gmail.com>
2021-03-09 07:15:47 +0100

    Constructeur  par défault herité

--- Personne/src/org/afpajava/Emplyee.java
+++ Personne/src/org/afpajava/Emplyee.java
@@ -1,5 +1,26 @@
 package org.afpajava;
 
 public class Emplyee extends Personne{
+private double salary;
 
+    public Emplyee() {
+        super();
+   
+    }
+
+    public Emplyee(int identifier, String firstName, String lastName, String email,
+                   String adresse, String ville, double salary) {
+        super(identifier, firstName, lastName, email, adresse, ville);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary<0)
+            throw new RuntimeException("Salary > 0");
+        this.salary = salary;
+    }
 }
